3d3a8704fc9d15168d5154213ba0e8687c9354fe
Jack Mallers <jimmymowschess@gmail.com>
2015-10-05 18:09:14 -0500

    about to try and configure to test on my device

--- App/Utils/api.js
+++ App/Utils/api.js
@@ -1,10 +1,10 @@
 var api = {
    getMoments() {
-      var url = `http://localhost:3000/api/v1/moments/rand/5`;
+      var url = `https://shrouded-mountain-1579.herokuapp.com/api/v1/moments/rand/5`;
       return fetch(url).then((res) => res.json());
    },
    getMoment() {
-      var url = `http://localhost:3000/api/v1/moments/rand/1`;
+      var url = `https://shrouded-mountain-1579.herokuapp.com/api/v1/moments/rand/1`;
       return fetch(url).then((res) => res.json());
    }
 };
--- sports-api/db/seeds.rb
+++ sports-api/db/seeds.rb
@@ -9,10 +9,11 @@ puts "Destroying all moments"
 Moment.destroy_all
 puts "Moments destroyed"
 
-ytids = ["ZkfPYw7CK10", "awAq_7S08o0", "VDTKfhGYwrU"]
+ytids = ["ZkfPYw7CK10", "VDTKfhGYwrU", "mfebpLfAt8g", "awAq_7S08o0", "gCb9FcHBIYE", "_vKDygyOxH0", "vyL0FxS-F6E", "4fW3tX-UinQ", "ADvWhMnDgEE", "riMTb36G84A", "UT0r8wBL4QU", "4CR9kKGeE5Q", "U157X0jy5iw", "gUK9lG-7HTc", "GMuUBZ_DAeM", "qYscemhnf88", "XMrPjl-927Q", "1f20D9HRiDE", "cvGfV6qCiOI", "_Z-QfDiV2XA", "b7eZmKWW9s4", "xbdddwVP7X4", "nLhTCwor1YQ", "QEhNDUwksvU", "4SbJ4Xf-t5E", "PRkeahelZHM"]
 
 ytids.each do |ytid|
-   puts "Creating moment: #{ytid}"
-   Moment.create(ytid: ytid)
+   url = "https://www.youtube.com/watch?v=#{ytid}"
+   puts "Creating moment: #{ytid}..."
+   Moment.create(ytid: url)
    puts "#{ytid} created"
 end
