a81adff047a3b80823b634461aae55e16aba85b5
Jack Mallers <jimmymowschess@gmail.com>
2015-10-16 14:56:56 -0500

    updated the api web scraper to trim queries until it finds results

--- sports-api/app/models/moment.rb
+++ sports-api/app/models/moment.rb
@@ -16,19 +16,22 @@ class Moment < ActiveRecord::Base
       youtube = Youtube.new(self.ytid)
       video = youtube.video
       self.title = video['items'][0]['snippet']['title']
-      puts "title: #{title}"
 
       # Search google for relevant wiki page
       query = self.title + " site:wikipedia.org"
-      puts "query: #{query}"
       webscraper = Webscraper.new
       wiki_url = webscraper.google_search(query)
-      puts "wiki url: #{wiki_url}"
-      puts "#{wiki_url.split('/')[-1]}"
-      wiki_title = URI.unescape(wiki_url.split('/')[-1])
+      moment_title = self.title
+      # If the original query doesn't work this while loop will trim it down until it gets results
+      while wiki_url.kind_of?(Array)
+         moment_title = moment_title.split(' ')
+         moment_title.pop
+         moment_title = moment_title.join(' ')
+         moment_query = moment_title + " site:wikipedia.org"
+         wiki_url = webscraper.google_search(moment_query)
+      end
 
-      puts "wiki title: #{wiki_title}"
-      puts "wiki: #{wiki}"
+      wiki_title = URI.unescape(wiki_url.split('/')[-1])
 
       # Get first paragraph from wiki page
       page = Wikipedia.find( wiki_title )
--- todo.md
+++ todo.md
@@ -1,3 +1,5 @@
-1) Make moments a prop to Main and not on state.
+1) Launch Screen needs to be better
 
-2) Make tinder like swiper card instead pf next button
+2) Need a logo for my app image thing
+
+3) Make sure you are not able to turn the app
